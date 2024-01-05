bdfde6c2926072b66a01bf4d517f3cddfe348a9c
Ivor Rudenko <rudenko23@gmail.com>
2024-01-05 22:11:44 +0200

    змінив run.yml add class minus

---
+++ src/main/java/RIO/example/SumCalculatorMinus.java
@@ -0,0 +1,14 @@
+package RIO.example;
+
+public class SumCalculatorMinus {
+    private int n;
+
+    public int sum(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n має бути більше за 0");
+        }
+        // формула для обчислення суми арифметичної погресії числа п
+        return (n * (n + 2));
+    }
+
+}
